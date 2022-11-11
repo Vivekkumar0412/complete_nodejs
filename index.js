cd07ea3510cec9cc86e8bdf324895adaf6b0f48c
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-11-11 13:12:13 +0530

    readFileSynch in node js

--- index.js
+++ index.js
@@ -3,3 +3,13 @@
 let fs = require("fs");
 
 fs.writeFileSync("text.txt","Hi there I am vivek")
+
+fs.readFileSync("text.txt")
+
+// but it will out put a buffer data.
+
+// #### BUFFER data is an additional feature in node js that was not present in js it basically stores the bytes.
+
+// to convert the buffer data to normal code we have two methods.
+
+console.log(fs.readFileSync("text.txt").toString());
