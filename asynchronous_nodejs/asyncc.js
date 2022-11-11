b709f5582e6a120d82bb16d48f159c484a3872d9
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-11-11 13:27:06 +0530

    writeFile in asynchrounous programming  in node js

--- asynchronous_nodejs/asyncc.js
+++ asynchronous_nodejs/asyncc.js
@@ -5,4 +5,9 @@ fs.writeFile("text.txt","I am an asynchronous file",(err)=>{
     console.log(err)
 })
 
-//
+// here also if i use write file sync again the whole data will be overwritten.
+
+fs.writeFile("text.txt","Hi iam vivek kumar",(err)=>{
+    console.log("overwriten")
+    console.log(err)
+})
