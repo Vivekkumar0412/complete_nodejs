64919e29561779d2a86a3d06ea8b8a3cfa52ce4d
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-11-11 13:46:48 +0530

    os.totalmem() in os module in node js

--- os_module/osmodule.js
+++ os_module/osmodule.js
@@ -6,3 +6,8 @@ console.log(os.arch())
 
 console.log(os.hostname());
 // it will tell the host name
+
+console.log(os.totalmem()/1024/1024/1034);
+
+// it will tell the total memory of ram of our system
+// we divided it by 1024 three  times because it return the results in bytes and we converted it into gb
