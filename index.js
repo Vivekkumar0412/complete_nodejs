b5a8a9376c81d82a991b08d32b86e2ef437e2b19
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-11-11 13:52:58 +0530

    path module in node js

--- index.js
+++ index.js
@@ -31,4 +31,5 @@ fs.appendFileSync("text.txt"," I am learning node js so that i can grab a job.")
 console.log(fs.readFileSync("text.txt","utf-8"))
 
 // fs.mkdirSync("asynchronous_nodejs")
-fs.mkdirSync("os_module")
+// fs.mkdirSync("os_module")
+fs.mkdirSync("path_module")
