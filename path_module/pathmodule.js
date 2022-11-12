1ecf3f5f8251a55b967907f23debb50245378258
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-11-12 09:41:04 +0530

    path module in node js

--- path_module/pathmodule.js
+++ path_module/pathmodule.js
@@ -4,4 +4,8 @@ let path = require("path");
 
 console.log(path.parse("C:/Users/vivek/Desktop/nodejs/path_module/pathmodule.js"))
 // path.parse("C:/Users/vivek/Desktop/nodejs/path_module/pathmodule.js") basicalyy return an object which comprises of root , dir, base, extension and name of the folder.
+
+
 console.log(path.dirname("C:/Users/vivek/Desktop/nodejs/path_module/pathmodule.js"))
+
+// path.dirname will basically return the path name 
