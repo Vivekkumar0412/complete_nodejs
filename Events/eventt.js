43763e04646a57347db42bc8aae75e57a29ed44a
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-11-17 19:14:45 +0530

    EventEmitter , .on() and .emit() properties of Events module in node js

--- Events/eventt.js
+++ Events/eventt.js
@@ -4,3 +4,10 @@ let EventEmitter = require("events");
 
 let myEvent = new EventEmitter();
 // console.log(myEvent);
+
+myEvent.on("myName",(a,b)=>{
+    console.log("my name is vivek singh rajput");
+    console.log(`the sum is : ${a+b} and product is : ${a*b}`)
+})
+
+myEvent.emit("myName",9,9);
