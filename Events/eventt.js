483618ccbf4e40e183cc574eb0cc10fb72544bc2
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-11-17 19:46:54 +0530

    this keyword in events in node js

--- Events/eventt.js
+++ Events/eventt.js
@@ -14,12 +14,16 @@ myEvent.emit("myName",9,9);
 
 myEvent.on("dt",function(name,age,salary){
     console.log(`Hi there I am ${name} my age is ${age} and my current salary is ${salary }`)
-    console.log(this)
+
+    // console.log(this)
 })
 
-myEvent.on("dt",function(name,age,salary){
-    console.log(`Hi there I am ${name} my age is ${age} and my current salary is ${salary }`)
-    console.log(this)
-})
+// myEvent.on("dt",function(name,age,salary){
+//     console.log(`Hi there I am ${name} my age is ${age} and my current salary is ${salary }`)
+//     // console.log(this)
+// })
 
+myEvent.emit("dt","Vivek",23,"1.5 cr per annum")
+myEvent.emit("dt","Vivek",23,"1.5 cr per annum")
+myEvent.emit("dt","Vivek",23,"1.5 cr per annum")
 myEvent.emit("dt","Vivek",23,"1.5 cr per annum")
